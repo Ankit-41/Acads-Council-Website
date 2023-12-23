38e07c23d56103b30c5599e6fd0e40a774ddbc10
Piyush_Chavan <piyushchavangd2004@gmail.com>
2023-12-23 18:13:30 +0530

    Footer Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      
+      <Navbar /><br /><br /><br />
+      <Footer />
     </div>
   );
 }
