21cf01776837f061accf0940e10c440732e7d7e1
Vaibhav-Bothra <vaibhavbothra2004@gmail.com>
2023-12-25 15:15:08 +0530

    added Contacts page component.

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,7 +1,356 @@
-import React from 'react'
+import React from "react";
 
 export default function Contacts() {
   return (
-    <div>Contacts</div>
-  )
+    <div>
+      <h1>Contacts</h1>
+      <div>
+        <br />
+
+        <div className="row">
+          <div className="col-md-3 feature"></div>
+          <div className="col-md-3 feature">
+            <h4>Sanchit Chaba</h4>
+            <p>
+              General Secretary
+              <br />
+              Academic Affairs
+            </p>
+            <p>xxx@gmail.com</p>
+            <p>989777xxxx</p>
+          </div>
+          <div className="col-md-3 feature">
+            <h4>Tushar Tembhurne</h4>
+            <p>
+              Deputy General Secretary
+              <br />
+              Academic Affairs
+            </p>
+            <p>yyy@gmail.com</p>
+            <p>987766xxxx</p>
+          </div>
+
+          <div className="col-md-3 feature"></div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="col-md-4 feature">
+            <h4>Harshit Singhai</h4>
+            <p>
+              Institute Secretary
+              <br />
+              Academics and Research
+            </p>
+            <p>harshits20@iitk.ac.in</p>
+            <p>9950429349</p>
+          </div>
+
+          <div className="col-md-4 feature">
+            <h4>Atirek Aryan</h4>
+            <p>
+              UG Institute Secretary
+              <br />
+              Career Development and International Relation
+            </p>
+            <p>aryana20@iitk.ac.in</p>
+            <p>8521251414</p>
+          </div>
+          <div className="col-md-4 feature">
+            <h4>Toushif Alam</h4>
+            <p>
+              PG Institute Secretary
+              <br />
+              Career Development and International Relation
+            </p>
+            <p>toushif22@iitk.ac.in</p>
+            <p>8001793374</p>
+          </div>
+        </div>
+        <br />
+        <h3>Career Development Wing</h3>
+        <hr />
+
+        <div className="row">
+          <div className="col-md-4 feature"></div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="col-md-3 feature">
+            <h4>Lalit kumar</h4>
+            <p>
+              PG Manager
+              <br />
+              Career Development
+            </p>
+            <p>lalitkc22@iitk.ac.in</p>
+            <p>7004925569</p>
+          </div>
+
+          <div className="col-md-3 feature">
+            <h4>Shubham Patil</h4>
+            <p>
+              PG Manager
+              <br />
+              Career Development
+            </p>
+            <p>pshubham22@iitk.ac.in</p>
+            <p>9011472545</p>
+          </div>
+
+          <div className="col-md-3 feature">
+            <h4>Tushar Agarwal</h4>
+            <p>
+              PG Manager
+              <br />
+              Career Development
+            </p>
+            <p>tusharag22@iitk.ac.in</p>
+            <p>7381229392</p>
+          </div>
+
+          <div className="col-md-3 feature">
+            <h4>Ashutosh Jha</h4>
+            <p>
+              PG Manager
+              <br />
+              Career Development
+            </p>
+            <p>ashutoshj22@iitk.ac.in</p>
+            <p>7738458703</p>
+          </div>
+
+          <div className="col-md-3 feature"></div>
+        </div>
+        <br />
+
+        <div className="row">
+          <div className="col-md-3 feature">
+            <h4>Utkarsh gupta</h4>
+            <p>
+              UG Manager
+              <br />
+              Career Development
+            </p>
+            <p>utkarshg21@iitk.ac.in </p>
+            <p>6307550041</p>
+          </div>
+
+          <div className="col-md-3 feature">
+            <h4>Aviral Upadhyay</h4>
+            <p>
+              UG Manager
+              <br />
+              Career Development
+            </p>
+            <p>aviral21@iitk.ac.in</p>
+            <p>9794978333</p>
+          </div>
+
+          <div className="col-md-3 feature">
+            <h4>Saumya Gupta</h4>
+            <p>
+              UG Manager
+              <br />
+              Career Development
+            </p>
+            <p>saumyag21@iitk.ac.in</p>
+            <p>9935010214</p>
+          </div>
+
+          <div className="col-md-3 feature">
+            <h4>Arpit Anand</h4>
+            <p>
+              UG Manager
+              <br />
+              Career Development
+            </p>
+            <p>arpit21@iitk.ac.in</p>
+            <p>9389188193</p>
+          </div>
+        </div>
+        <br />
+
+        <h3>International Relations Wing</h3>
+        <hr />
+        <br />
+        <div className="row">
+          <div className="col-md-4 feature">
+            <h4>Aashish Shah</h4>
+            <p>
+              PG Manager
+              <br />
+              International Relations
+            </p>
+            <p>aashishs22@iitk.ac.in</p>
+            <p>9122276258</p>
+          </div>
+
+          <div className="col-md-4 feature">
+            <h4>Abhirami A M</h4>
+            <p>
+              PG Manager
+              <br />
+              International Relations
+            </p>
+            <p>abhiramia21@iitk.ac.in</p>
+            <p>8330091941</p>
+          </div>
+
+          <div className="col-md-4 feature">
+            <h4>Soumyadeep Sur</h4>
+            <p>
+              PG Manager
+              <br />
+              International Relations
+            </p>
+            <p>soumyadeep22@iitk.ac.in</p>
+            <p>9163818042</p>
+          </div>
+
+          <div className="col-md-3 feature"></div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="col-md-3 feature">
+            <h4>Jai Verma</h4>
+            <p>
+              UG Manager
+              <br />
+              International Relations
+            </p>
+            <p>jaiverma21@iitk.ac.in</p>
+            <p>8469062471</p>
+          </div>
+
+          <div className="col-md-3 feature">
+            <h4>Priyanshu Tiwari</h4>
+            <p>
+              UG Manager
+              <br />
+              International Relations
+            </p>
+            <p>priyanshut21@iitk.ac.in</p>
+            <p>8795950384</p>
+          </div>
+
+          <div className="col-md-3 feature">
+            <h4>Ananya Bist</h4>
+            <p>
+              UG Manager
+              <br />
+              International Relations
+            </p>
+            <p>ananyab21@iitk.ac.in</p>
+            <p>6392838486</p>
+          </div>
+          <div className="col-md-3 feature">
+            <h4>Drashti Modi</h4>
+            <p>
+              UG Manager
+              <br />
+              International relations
+            </p>
+            <p>drashti21@iitk.ac.in</p>
+            <p>9979873894</p>
+          </div>
+        </div>
+
+        <br />
+        <h3>Academics Wing</h3>
+        <hr />
+        <br />
+
+        <div className="row">
+          <div className="col-md-3 feature">
+            <h4>Aqdas Alam</h4>
+            <p>
+              PG Manager
+              <br />
+              PG Academia
+            </p>
+            <p>aqdasa22@iitk.ac.in</p>
+            <p>9007363286</p>
+          </div>
+
+          <div className="col-md-3 feature">
+            <h4>Omveer Singh</h4>
+            <p>
+              PG Manager
+              <br />
+              PG Academia
+            </p>
+            <p>somveer21@iitk.ac.in</p>
+            <p>9711926080</p>
+          </div>
+
+          <div className="col-md-3 feature">
+            <h4>Soumya Dubey</h4>
+            <p>
+              PG Manager
+              <br />
+              PG Academia
+            </p>
+            <p>soumyad22@iitk.ac.in</p>
+            <p>9179232425</p>
+          </div>
+          <div className="col-md-3 feature">
+            <h4>Subrata Das</h4>
+            <p>
+              PG Manager
+              <br />
+              PG Academia
+            </p>
+            <p>subratadas22@iitk.ac.in</p>
+            <p>8250173478</p>
+          </div>
+        </div>
+        <br />
+
+        <div className="row">
+          <div className="col-md-3 feature"></div>
+          <div className="col-md-3 feature">
+            <h4>Medha Srivastava</h4>
+            <p>
+              Manager
+              <br />
+              UG Academia
+            </p>
+            <p>medhas21@iitk.ac.in</p>
+            <p>7393028264</p>
+          </div>
+
+          <div className="col-md-3 feature">
+            <h4>Siddharth Maurya</h4>
+            <p>
+              Manager
+              <br />
+              UG Academia
+            </p>
+            <p>smaurya21@iitk.ac.in</p>
+            <p>9328717328</p>
+          </div>
+          <div className="col-md-3 feature"></div>
+        </div>
+
+        <br />
+        <h3>Web Development Wing</h3>
+        <hr />
+        <br />
+        <div className="row">
+          <div className="col-md-4 feature"></div>
+          <div className="col-md-4 feature">
+            <h4>Saksham Agarwal</h4>
+            <p>
+              UG Manager
+              <br />
+              Web Development
+            </p>
+            <p>sakshamag21@iitk.ac.in</p>
+            <p>8279942685</p>
+          </div>
+          <div className="col-md-4 feature"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
